d3892f847638e0399bf9f7bee386479372ff64f1
Rebecca Siegal <becca.siegal@gmail.com>
2019-09-11 15:18:13 -0400

    Fixed authorization error

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ app.use(helmet())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Bearer')
-  if (!authToken || authToken !== apiToken) {
+  const authToken = req.get('Authorization')
+  console.log(authToken.split(" "));
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
   next()
